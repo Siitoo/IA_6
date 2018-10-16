313ee05bea38a39b8d8b54ff8e46d7d235196e45
Siitoo <alfonsosanchez1896@gmail.com>
2018-10-16 11:17:14 +0200

    add navmesh

---
+++ AI_CLASS06/Assets/navme.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class navme : MonoBehaviour
+{
+
+    public NavMeshAgent[] tanks;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        foreach (NavMeshAgent tank in tanks)
+        {
+            tank.destination = transform.position;
+        }
+
+    }
+}
+
